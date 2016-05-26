ca1917bd902bba6bf81238fe793b4f0986ed0d98
Shawn Aten <shawnmaten@gmail.com>
2016-05-25 21:53:15 -0500

    Validate that requests to /sms come from Twilio.

--- controllers/sms_handler.py
+++ controllers/sms_handler.py
@@ -1,17 +1,37 @@
+import logging
+
+import twilio.twiml
 import webapp2
+from twilio.util import RequestValidator
 
 import controllers.command_controller as command
 import controllers.create_controller as create
 import controllers.delete_controller as delete
 import controllers.onboard_controller as onboard
 import models.account as account_model
+import strings
 from models.account import Account
 
 
 class SMSHandler(webapp2.RequestHandler):
     def get(self):
 
-        phone = self.request.get("From")
+        # Sending messages separately, send empty TwiML for now.
+        twiml = twilio.twiml.Response()
+        self.response.write(twiml)
+
+        # Validate that request came from Twilio service.
+        validator = RequestValidator(strings.AUTH_TOKEN)
+        url = self.request.url
+        url = url.replace("http", "https")
+        signature = self.request.headers.get("X-Twilio-Signature", "")
+
+        if not validator.validate(url, {}, signature):
+            logging.warn("Request did not come from Twilio.")
+            return
+
+        phone = self.request.get("From", None)
+
         account = Account.query().filter(Account.phone == phone).get()
 
         if account is None:
@@ -24,5 +44,3 @@ class SMSHandler(webapp2.RequestHandler):
             onboard.controller(self.request, account)
         elif account.state == account_model.STATE_DELETE:
             delete.controller(self.request, account)
-
-        self.response.write("")
