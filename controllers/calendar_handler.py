6cd3e1d8cfce75cb3db48be295b2ca66ee60ba26
Shawn Aten <shawnmaten@gmail.com>
2016-05-25 21:55:19 -0500

    Fetch ICS file from Google.

--- controllers/calendar_handler.py
+++ controllers/calendar_handler.py
@@ -1,7 +1,19 @@
 import webapp2
+from google.appengine.api import urlfetch
 
 
 class CalendarHandler(webapp2.RequestHandler):
     def get(self):
+
+        calendar_url = "https://calendar.google.com/calendar/ical/utsaspectrum%40gmail.com/public/basic.ics"
+
+        try:
+            result = urlfetch.fetch(calendar_url)
+            if result.status_code == 200:
+                logging.info(result.content)
+            else:
+                self.response.status_code = result.status_code
+        except urlfetch.Error:
+            logging.exception('caught exception fetching calendar')
 
         self.response.write("")
