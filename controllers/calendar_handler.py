fff1f77b018e8a0f3125ae887a7fd61cf73efb5d
Shawn Aten <shawnmaten@gmail.com>
2016-05-27 12:45:27 -0500

    Change to use Google Calendar API instead of fetching ICS file.

--- controllers/calendar_handler.py
+++ controllers/calendar_handler.py
@@ -1,19 +1,60 @@
+import datetime
+import logging
+
 import webapp2
-from google.appengine.api import urlfetch
+from apiclient import discovery
+from httplib2 import Http
+from oauth2client.contrib.appengine import AppAssertionCredentials
 
 
 class CalendarHandler(webapp2.RequestHandler):
+
     def get(self):
 
-        calendar_url = "https://calendar.google.com/calendar/ical/utsaspectrum%40gmail.com/public/basic.ics"
+        credentials = AppAssertionCredentials("https://www.googleapis.com/auth/calendar.readonly")
+        http_auth = credentials.authorize(Http())
+        cal_service = discovery.build('calendar', 'v3', http=http_auth)
 
-        try:
-            result = urlfetch.fetch(calendar_url)
-            if result.status_code == 200:
-                logging.info(result.content)
-            else:
-                self.response.status_code = result.status_code
-        except urlfetch.Error:
-            logging.exception('caught exception fetching calendar')
+        events = []
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        events_result = cal_service.events().list(calendarId="utsaspectrum@gmail.com", timeMin=now).execute()
+        events += events_result.get('items', [])
+        next_page_token = events_result.get('nextPageToken', None)
 
-        self.response.write("")
+        while next_page_token:
+            events_result = cal_service.events().list(calendarId="utsaspectrum@gmail.com", timeMin=now,
+                                                      pageToken=next_page_token).execute()
+            events += events_result.get('items', [])
+            next_page_token = events_result.get('nextPageToken', None)
+
+        next_sync_token = events_result.get("nextSyncToken", None)
+
+        logging.info("nextSyncToken: " + next_sync_token)
+
+        for event in events:
+            logging.info(event.get('summary'))
+    
+#    def get(self):
+
+#        calendar_url = "https://calendar.google.com/calendar/ical/utsaspectrum%40gmail.com/public/basic.ics"
+
+#        result = urlfetch.fetch(calendar_url, validate_certificate=True)
+
+#        if result.status_code != 200:
+#            logging.exception("Error syncing calendar.")
+#            self.response.status = result.status_code
+#            return
+
+#        cal = Calendar.from_ical(result.content)
+
+#        central = timezone('US/Central')
+#        current = datetime.now(tz=central)
+#        day = current.date()
+
+#        for event in cal.walk("vevent"):
+#            start = event.decoded('dtstart')
+#            end = event.decoded('dtend')
+#            summary = event.decoded('summary')
+
+#            if type(start) is datetime and start >= current or type(start) is date and start >= day:
+#                logging.info("%s: %s to %s", summary, start, end)
